73742b0e50b40f9d8042c90f76fdd392e495c456
EhSlashEh <anthonyalanb@hotmail.com>
2024-08-31 21:06:21 -0300

    Modern api

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,39 @@
-'use strict';
-
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
 
+  app.route('/api/threads/:board').post(async (req, res) => {
+    try {
+      const { text, delete_password } = req.body;
+      let board = req.body.board;
+      if (!board) {
+        board = req.params.board;
+      }
+
+      const newThread = new ThreadModel({
+        text: text,
+        delete_password: delete_password,
+        replies: [],
+      });
+
+      let Boarddata = await BoardModel.findOne({ name: board });
+
+      if (!Boarddata) {
+        const newBoard = new BoardModel({
+          name: board,
+          threads: [],
+        });
+        newBoard.threads.push(newThread);
+        const data = await newBoard.save();
+        res.json(newThread);
+      } else {
+        Boarddata.threads.push(newThread);
+        const data = await Boarddata.save();
+        res.json(newThread);
+      }
+    } catch (err) {
+      console.log(err);
+      res.send("There was an error saving in post");
+    }
+  });
+
+  app.route('/api/replies/:board');
 };
