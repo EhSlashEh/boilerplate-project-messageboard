19416e22f8563433791aada30f2592b3db6c56b8
EhSlashEh <anthonyalanb@hotmail.com>
2024-09-01 01:32:54 -0300

    working on it

--- routes/api.js
+++ routes/api.js
@@ -6,54 +6,44 @@ const ReplyModel = require("../models").Reply;
 
 module.exports = function (app) {
   app.route("/api/threads/:board")
-  .post((req, res) => {
-    const { text, delete_password } = req.body;
-    let board = req.body.board;
-    if (!board) {
-      board = req.params.board;
-    }
-    const newThread = new ThreadModel({
-      text: text,
-      delete_password: delete_password,
-      replies: [],
-    });
+    .post(async (req, res) => {
+      try {
+        const { text, delete_password } = req.body;
+        let board = req.body.board;
+        if (!board) {
+          board = req.params.board;
+        }
+        const newThread = new ThreadModel({
+          text: text,
+          delete_password: delete_password,
+          replies: [],
+        });
 
-    BoardModel.findOne({ name: board }, (err, Boarddata) => {
-      if (err) {
-        console.error("Error finding board:", err);
+        let boardData = await BoardModel.findOne({ name: board }).exec();
+        if (!boardData) {
+          const newBoard = new BoardModel({
+            name: board,
+            threads: [],
+          });
+
+          newBoard.threads.push(newThread);
+          await newBoard.save();
+          return res.json(newThread);
+        } else {
+          boardData.threads.push(newThread);
+          await boardData.save();
+          return res.json(newThread);
+        }
+      } catch (err) {
+        console.error("Error:", err);
         return res.status(500).send("Internal Server Error");
       }
+    })
 
-      if (!Boarddata) {
-        const newBoard = new BoardModel({
-          name: board,
-          threads: [],
-        });
-
-        newBoard.threads.push(newThread);
-        newBoard.save((err, data) => {
-          if (err) {
-            console.error("Error saving new board:", err);
-            return res.status(500).send("Internal Server Error");
-          }
-          res.json(newThread);
-        });
-      } else {
-        Boarddata.threads.push(newThread);
-        Boarddata.save((err, data) => {
-          if (err) {
-            console.error("Error saving thread to existing board:", err);
-            return res.status(500).send("Internal Server Error");
-          }
-          res.json(newThread);
-        });
-      }
-    });
-  })
-
-    .get((req, res) => {
-      const board = req.params.board;
-      BoardModel.findOne({ name: board }, (err, data) => {
+    .get(async (req, res) => {
+      try {
+        const board = req.params.board;
+        const data = await BoardModel.findOne({ name: board }).exec();
         if (!data) {
           console.log("No board with this name");
           res.json({ error: "No board with this name" });
@@ -82,75 +72,86 @@ module.exports = function (app) {
           });
           res.json(threads);
         }
-      });
+      } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Internal Server Error");
+      }
     })
-    .put((req, res) => {
-      console.log("put", req.body);
-      const { report_id } = req.body;
-      const board = req.params.board;
-      BoardModel.findOne({ name: board }, (err, boardData) => {
+
+    .put(async (req, res) => {
+      try {
+        console.log("put", req.body);
+        const { report_id } = req.body;
+        const board = req.params.board;
+        const boardData = await BoardModel.findOne({ name: board }).exec();
         if (!boardData) {
-          res.json("error", "Board not found");
+          res.json({ error: "Board not found" });
         } else {
           const date = new Date();
           let reportedThread = boardData.threads.id(report_id);
           reportedThread.reported = true;
           reportedThread.bumped_on = date;
-          boardData.save((err, updatedData) => {
-            res.send("Success");
-          });
+          await boardData.save();
+          res.send("Success");
         }
-      });
+      } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Internal Server Error");
+      }
     })
-    .delete((req, res) => {
-      console.log("delete", req.body);
-      const { thread_id, delete_password } = req.body;
-      const board = req.params.board;
-      BoardModel.findOne({ name: board }, (err, boardData) => {
+
+    .delete(async (req, res) => {
+      try {
+        console.log("delete", req.body);
+        const { thread_id, delete_password } = req.body;
+        const board = req.params.board;
+        const boardData = await BoardModel.findOne({ name: board }).exec();
         if (!boardData) {
-          res.json("error", "Board not found");
+          res.json({ error: "Board not found" });
         } else {
           let threadToDelete = boardData.threads.id(thread_id);
           if (threadToDelete.delete_password === delete_password) {
             threadToDelete.remove();
+            await boardData.save();
+            res.send("Success");
           } else {
             res.send("Incorrect Password");
-            return;
           }
-          boardData.save((err, updatedData) => {
-            res.send("Success");
-          });
         }
-      });
+      } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Internal Server Error");
+      }
     });
 
-  app
-    .route("/api/replies/:board")
-    .post((req, res) => {
-      console.log("thread", req.body);
-      const { thread_id, text, delete_password } = req.body;
-      const board = req.params.board;
-      const newReply = new ReplyModel({
-        text: text,
-        delete_password: delete_password,
-      });
-      BoardModel.findOne({ name: board }, (err, boardData) => {
+  app.route("/api/replies/:board")
+    .post(async (req, res) => {
+      try {
+        console.log("thread", req.body);
+        const { thread_id, text, delete_password } = req.body;
+        const board = req.params.board;
+        const newReply = new ReplyModel({ text, delete_password });
+        const boardData = await BoardModel.findOne({ name: board }).exec();
         if (!boardData) {
-          res.json("error", "Board not found");
+          res.json({ error: "Board not found" });
         } else {
           const date = new Date();
           let threadToAddReply = boardData.threads.id(thread_id);
           threadToAddReply.bumped_on = date;
           threadToAddReply.replies.push(newReply);
-          boardData.save((err, updatedData) => {
-            res.json(updatedData);
-          });
+          await boardData.save();
+          res.json(newReply);
         }
-      });
+      } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Internal Server Error");
+      }
     })
-    .get((req, res) => {
-      const board = req.params.board;
-      BoardModel.findOne({ name: board }, (err, data) => {
+
+    .get(async (req, res) => {
+      try {
+        const board = req.params.board;
+        const data = await BoardModel.findOne({ name: board }).exec();
         if (!data) {
           console.log("No board with this name");
           res.json({ error: "No board with this name" });
@@ -159,12 +160,17 @@ module.exports = function (app) {
           const thread = data.threads.id(req.query.thread_id);
           res.json(thread);
         }
-      });
+      } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Internal Server Error");
+      }
     })
-    .put((req, res) => {
-      const { thread_id, reply_id } = req.body;
-      const board = req.params.board;
-      BoardModel.findOne({ name: board }, (err, data) => {
+
+    .put(async (req, res) => {
+      try {
+        const { thread_id, reply_id } = req.body;
+        const board = req.params.board;
+        const data = await BoardModel.findOne({ name: board }).exec();
         if (!data) {
           console.log("No board with this name");
           res.json({ error: "No board with this name" });
@@ -174,19 +180,21 @@ module.exports = function (app) {
           let reply = thread.replies.id(reply_id);
           reply.reported = true;
           reply.bumped_on = new Date();
-          data.save((err, updatedData) => {
-            if (!err) {
-              res.send("Success");
-            }
-          });
+          await data.save();
+          res.send("Success");
         }
-      });
+      } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Internal Server Error");
+      }
     })
-    .delete((req, res) => {
-      const { thread_id, reply_id, delete_password } = req.body;
-      console.log("delete reply body", req.body);
-      const board = req.params.board;
-      BoardModel.findOne({ name: board }, (err, data) => {
+
+    .delete(async (req, res) => {
+      try {
+        const { thread_id, reply_id, delete_password } = req.body;
+        console.log("delete reply body", req.body);
+        const board = req.params.board;
+        const data = await BoardModel.findOne({ name: board }).exec();
         if (!data) {
           console.log("No board with this name");
           res.json({ error: "No board with this name" });
@@ -196,16 +204,15 @@ module.exports = function (app) {
           let reply = thread.replies.id(reply_id);
           if (reply.delete_password === delete_password) {
             reply.remove();
+            await data.save();
+            res.send("Success");
           } else {
             res.send("Incorrect Password");
-            return;
           }
-          data.save((err, updatedData) => {
-            if (!err) {
-              res.send("Success");
-            }
-          });
         }
-      });
+      } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Internal Server Error");
+      }
     });
 };
